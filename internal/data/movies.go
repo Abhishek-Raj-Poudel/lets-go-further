c6618846eac230b7f53bf24d6880ed6aeb9e4bdc
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2025-10-30 22:21:23 +0545

    Ch 4.5 Validating Json input

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,22 +1,33 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"greenlight.abhishekrajpoudel.com.np/internal/validator"
+)
 
 // NOTE: It’s crucial to point out here that all the fields in our Movie struct are exported (i.e. start with a capital letter),which is necessary for them to be visible to Go’s encoding/json package.
 // Any fields which aren’t exported won’t be included when encoding a struct to JSON.
 type Movie struct {
 	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"-"` // Use the - directive
+	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
-	Year      int32     `json:"year,omitempty"` // Add the omitempty directive
-	// Runtime   int32     `json:"runtime,omitempty"` // Add the omitempty directive
-	// Use the Runtime type instead of int32. Note that the omitempty directive will
-	// still work on this: if the Runtime field has the underlying value 0, then it will
-	// be considered empty and omitted -- and the MarshalJSON() method we just made
-	// won't be called at all.
-	Runtime Runtime  `json:"runtime,omitempty"`
-	Genres  []string `json:"genres,omitempty"` // Add the omitempty directive
-	Version int32    `json:"version"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   Runtime   `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
 }
 
-//NOTE: Hint: If you want to use omitempty and not change the key name then you can leave it blank in the struct tag — like this: json:",omitempty". Notice that the leading comma is still required.
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
