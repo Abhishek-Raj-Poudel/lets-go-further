2b31e313c7a619ff3a3cc89c0b8b9fb3bdddb177
Abhishek-Raj-Poudel <abhishekrajpoudel@gmail.com>
2025-12-14 23:23:48 +0545

    ch_7.4_fetching_data

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"net/http"
-	"time"
-
 	"greenlight.abhishekrajpoudel.com.np/internal/data"
 	"greenlight.abhishekrajpoudel.com.np/internal/validator"
+	"net/http"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -67,24 +66,24 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	id, err := app.readIDParam(r)
 	if err != nil {
-		// Use the new notFoundResponse() helper.
 		app.notFoundResponse(w, r)
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelop{"movie": movie}, nil)
 
 	if err != nil {
-		// Use the new serverErrorResponse() helper.
 		app.serverErrorResponse(w, r, err)
 	}
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/lib/pq"
@@ -35,7 +36,36 @@ func (m MovieModel) Insert(movie *Movie) error {
 
 // Define a MovieModel struct type which wraps a sql.DB connection pool.
 func (m MovieModel) Get(id int64) (*Movie, error) {
-	return nil, nil
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	query := `
+	SELECT id, created_at, title, year, runtime, genres, version
+	FROM movies
+	where id = $1
+	`
+	var movie Movie
+
+	err := m.DB.QueryRow(query, id).Scan(
+		&movie.ID,
+		&movie.CreatedAt,
+		&movie.Title,
+		&movie.Year,
+		&movie.Runtime,
+		pq.Array(&movie.Genres),
+		&movie.Version,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &movie, nil
 }
 
 // Add a placeholder method for updating a specific record in the movies table.
