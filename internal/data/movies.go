8455be9295bc64e88a6c923407ae123afface360
Abhishek-Raj-Poudel <abhishekrajpoudel@gmail.com>
2025-12-27 22:09:48 +0545

    ch_9.7_paginating_lists

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -160,13 +160,6 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 }
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	// query := `
-	// SELECT id, created_at, title, year, runtime, genres, version
-	// FROM movies
-	// WHERE (to_tsvector('simple',title) @@ plainto_tsquery('simple',$1) OR $1 = '')
-	// AND (genres @> $2 OR $2 = '{}')
-	// ORDER BY id
-	// `
 
 	query := fmt.Sprintf(`
 	SELECT id, created_at, title, year, runtime, genres, version
@@ -174,12 +167,15 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	WHERE (to_tsvector('simple',title) @@ plainto_tsquery('simple',$1) OR $1 = '')
 	AND (genres @> $2 OR $2 = '{}')
 	ORDER BY %s %s, id ASC
+	LIMIT $3 OFFSET $4
 	`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
+	args := []any{title, pq.Array(genres), filters.limit(), filters.offset()}
+
+	rows, err := m.DB.QueryContext(ctx, query, args...)
 	if err != nil {
 		return nil, err
 	}
